aa59530d57b798a3a7a16921a7e9d07c9b2f403b
Frank Casanova <frankcasanova.info@gmail.com>
2024-05-31 15:25:34 +0200

    header, aboutus and subheading done

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {images} from '../../constants';
+import './subheading.css';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
+const SubHeading = ( {title}) => (
+  <div className="app__subheading">
+    <p className='p__cormorant'>{title}</p> 
+    <img src={images.spoon} alt="spoon" className="spoon__img" />
   </div>
 );
 
